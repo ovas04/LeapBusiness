d7511b48c2d17cc27c291dde6a217d082cf9dc2c
Oskar Lobaton <oca040370@gmail.com>
2022-05-21 19:45:46 -0500

    Creacion clase Game

--- leapbusiness/scraps/Game.py
+++ leapbusiness/scraps/Game.py
@@ -3,7 +3,18 @@
 class Game:
 
     def __init__(self):
-        self.appId = None
+        
         self.data_metracritic = None
         self.data_steamCharts = []
         self.data_steamPriceHisoty = []
+
+
+    def set_data_metracritic(self, p_data_metracritic):
+        self.data_metracritic = p_data_metracritic
+
+    def set_data_steamCharts(self, p_data_steamCharts):
+        self.data_steamCharts = p_data_steamCharts
+    
+    def set_data_steamPriceHisoty(self, p_data_steamPriceHisoty):
+        self.data_steamPriceHisoty = p_data_steamPriceHisoty
+
